714ee977bca5370f260f91ce402ac12204b348e0
pmartinek <boss.martinek@seznam.cz>
2021-03-29 10:32:34 +0200

    Layer

--- ANN.py
+++ ANN.py
@@ -1,3 +1,6 @@
-import numpy
+import numpy as np
 from matplotlib import pyplot as plt
 
+class layer:
+    def __init__(self,n_inputs,n_neurons):
+        self.weights = 0.1 * np.random.randn((n_inputs,n_neurons))
