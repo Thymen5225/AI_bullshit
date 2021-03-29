87ca4c4f7e864fe6a05db44e20cc3d95d0d94438
pmartinek <boss.martinek@seznam.cz>
2021-03-29 12:01:56 +0200

    Activation and Loss

--- ANN.py
+++ ANN.py
@@ -7,8 +7,30 @@ class layer:
         self.biases = np.zeros((1,n_neurons))
     def forward(self,inputs):
         self.output = np.dot(inputs,self.weights) + self.biases
-layer1 = layer(2,2)
+
+class softmax:
+    def forward(self,inputs):
+        exp_values = np.exp(inputs - np.max(inputs,axis=1,keepdims=True))
+        probabilites = exp_values / np.sum(exp_values,axis=1,keepdims=True)
+        self.output  = probabilites
+class loss:
+    def forward(self,inputs,target):
+        sum = 0
+        for i in range(0,len(target)):
+            sum = sum + np.log(inputs[i]) * target[i]
+        self.output = sum
+
+#Trying
+
+layer1 = layer(2,10)
+activation1 = softmax()
 X = np.array([[1,2],
               [3,4]])
+target = np.zeros((10,1)).T
+target[0][0]=1
 layer1.forward(X)
-print(layer1.output)
+activation1.forward(layer1.output)
+probabilities = activation1.output
+loss1 = loss()
+loss1.forward(probabilities,target)
+print(loss1.output)
