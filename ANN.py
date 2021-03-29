a3a6d95a2299a766a228a71ac145da91a0011a5c
pmartinek <boss.martinek@seznam.cz>
2021-03-29 10:35:04 +0200

    Layer

--- ANN.py
+++ ANN.py
@@ -4,3 +4,4 @@ from matplotlib import pyplot as plt
 class layer:
     def __init__(self,n_inputs,n_neurons):
         self.weights = 0.1 * np.random.randn((n_inputs,n_neurons))
+        self.biases = np.zeros((1,n_neurons))
